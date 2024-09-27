6aba3bb3c75baad21058bbff3d26bd7074cc0401
Jason Healy <jason@healy.app>
2024-09-27 08:43:33 -0400

    create byte count library function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn count_bytes(text: &str) -> usize {
+    text.len()
 }
 
 #[cfg(test)]
-mod tests {
+mod count_bytes {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn succeeds() {
+        let input = "this is a test string";
+        let want = 21;
+        let got = count_bytes(input);
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn returns_zero_for_empty_input() {
+        let input = "";
+        let want = 0;
+        let got = count_bytes(input);
+        assert_eq!(got, want);
+    }
+
+    #[test]
+    fn correctly_counts_multibyte_characters() {
+        let input = "孫子";
+        let want = 6;
+        let got = count_bytes(input);
+        assert_eq!(got, want);
     }
 }
